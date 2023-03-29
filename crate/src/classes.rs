686391f13adb2aa17c526153781f209322706057
XOR·APPS <developers@xorapps.network>
2023-03-29 18:13:26 +0300

    Feat: Add ability to create a new JNI class using the `Class` struct
    Signed-off-by: XOR·APPS <developers@xorapps.network>

--- crate/src/classes.rs
+++ crate/src/classes.rs
@@ -1,3 +1,9 @@
+use jni::{
+    objects::{JClass, JObject, JValueOwned},
+    strings::JNIString,
+    JNIEnv,
+};
+
 use crate::{EasyJNIResult, EasyJniError};
 use std::collections::HashMap;
 
@@ -24,7 +30,28 @@ pub enum JavaType {
     String(String),
 }
 
-impl JavaType {
+impl<'local> JavaType {
+    pub fn to_jni(
+        &self,
+        env: &JNIEnv<'local>,
+        _: &JClass<'local>,
+    ) -> EasyJNIResult<JValueOwned<'local>> {
+        let outcome = match self {
+            Self::Byte(byte_value) => JValueOwned::from(*byte_value as i8),
+            Self::Short(short_value) => JValueOwned::from(*short_value),
+            Self::Int(int_value) => JValueOwned::from(*int_value),
+            Self::Long(long_value) => JValueOwned::from(*long_value),
+            Self::Float(float_value) => JValueOwned::from(*float_value),
+            Self::Double(double_value) => JValueOwned::from(*double_value),
+            Self::Boolean(bool_value) => JValueOwned::from(*bool_value),
+            Self::Void => JValueOwned::Void,
+            Self::String(string_value) => JValueOwned::from(env.new_string(string_value)?),
+            _ => return Err(EasyJniError::UnsupportedJavaType),
+        };
+
+        Ok(outcome)
+    }
+
     pub fn java_signature(&self) -> &str {
         match self {
             Self::Byte { .. } => "B",
@@ -67,16 +94,51 @@ impl JavaArray {
 }
 
 #[derive(Debug, PartialEq, PartialOrd, Clone)]
-pub enum Class {
-    Primitive {
-        name: String,
-        fields: Vec<(String, JavaType)>,
-    },
-    DataType {
-        name: String,
-        fields: Vec<(String, JavaType)>,
-    },
+pub struct Class<'local> {
+    name: &'local str,
+    fields: Vec<(&'local str, JavaType)>,
+}
+
+impl<'local> Default for Class<'local> {
+    fn default() -> Self {
+        Class {
+            name: "UNINITIALIZED_CLASS_NAME",
+            fields: vec![("UNINITALIZED_CLASS_FIELD", JavaType::Void)],
+        }
+    }
+}
+
+impl<'local> Class<'local> {
+    pub fn new() -> Self {
+        Class::default()
+    }
+    pub fn create(
+        self,
+        env: &mut JNIEnv<'local>,
+        java_class: JClass<'local>,
+    ) -> EasyJNIResult<JObject<'local>> {
+        let class = env.find_class(&self.name)?;
+
+        let object = env.alloc_object(class)?;
+
+        for (name, value) in &self.fields {
+            let field_name = JNIString::from(name);
+
+            env.set_field(
+                &object,
+                field_name,
+                value.java_signature(),
+                value.to_jni(&env, &java_class)?.borrow(),
+            )?;
+        }
+
+        Ok(object)
+    }
 }
 
 #[derive(Debug, Clone)]
 pub struct JavaCollection(HashMap<JavaType, JavaType>);
+
+fn foo<'local>(mut env: JNIEnv<'local>, java_class: JClass<'local>) {
+    let foo = Class::new().create(&mut env, java_class);
+}
